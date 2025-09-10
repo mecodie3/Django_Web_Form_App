2f8d4f84a1f609d66d0ad5afe50ddc7ea2abfa24
redroze <mecodie3@gmail.com>
2025-09-10 23:04:31 +0100

    Application Form Class to Get Data

---
+++ job_application/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+# class for the form
+class ApplicationForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    date = forms.DateField()
+    occupation = forms.CharField(max_length=80)
+
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 def index(request):
+    #get the data from the widgets
+    if request.method == "POST":
+        #ContactForm Class
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            #dictionary is created in cleaned_data like {"first_name":"John","last_name":"Smith"}
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name,last_name, email,date,occupation)
     return render(request,"index.html")
+
+
 
