3ec2459819318085dda6d2d7e0b03539cffbf3ad
redroze <mecodie3@gmail.com>
2025-09-11 15:48:47 +0100

    Customizing The Admin Interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+#Class to customize what we want to see in Admin Interface
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name","email")
+     #to add search bar widget
+    search_fields = ("first_name", "last_name", "email")
+    list_filter =("date","occupation")
+    ordering=("first_name",) #("-first_name",)-> minus for reverse order
+    #specify which field to read
+    readonly_fields=("occupation",)
+
+admin.site.register(Form,FormAdmin)
 
